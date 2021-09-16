aeb5c94b7b2c0febab4ca29c9e64a8aa686c26de
Poornima <poornima.dulanjalee@gmail.com>
2021-09-16 13:59:39 +0530

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,77 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Emerald shirt',
+            category: 'Shirts',
+            Price: 1000,
+            image: '/images/Shirt.jpg',
+            brand: 'Emerald',
+            rating: 4.5,
+            numReviews: 15,
+            description:'High quality product',
+    
+        },
+        {
+            _id: '2',
+            name: 'Nike shirt',
+            category: 'Shirts',
+            Price: 1200,
+            image: '/images/Shirt.jpg',
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 18,
+            description:'High quality product',
+    
+        },
+        {
+            _id: '3',
+            name: 'Emerald shirt',
+            category: 'Shirts',
+            Price: 5000,
+            image: '/images/Shirt.jpg',
+            brand: 'Emerald',
+            rating: 4.5,
+            numReviews: 19,
+            description:'High quality product',
+    
+        },
+        {
+            _id: '4',
+            name: 'Emerald shirt',
+            category: 'Shirts',
+            Price: 1050,
+            image: '/images/Shirt.jpg',
+            brand: 'Emerald',
+            rating: 3,
+            numReviews: 20,
+            description:'High quality product',
+    
+        },
+        {
+            _id: '5',
+            name: 'Adidas shirt',
+            category: 'Shirts',
+            Price: 1500,
+            image: '/images/Shirt.jpg',
+            brand: 'Adidas',
+            rating: 5,
+            numReviews: 10,
+            description:'High quality product',
+    
+        },
+        {
+            _id: '6',
+            name: 'Odel shirt',
+            category: 'Shirts',
+            Price: 2000,
+            image: '/images/Shirt.jpg',
+            brand: 'Odel',
+            rating: 4.5,
+            numReviews: 20,
+            description:'High quality product',
+    
+        },
+    ],
+};
+export default data;
